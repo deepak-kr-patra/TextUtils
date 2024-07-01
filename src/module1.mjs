26a43f9252dcb774ba1a2495b5dfd7e2d4b0e853
deepak-kr-patra <105375871+deepak-kr-patra@users.noreply.github.com>
2024-07-01 12:56:06 +0530

    removed unnecessary files

--- src/module1.mjs
+++
@@ -1,6 +0,0 @@
-import as, {a, b, d} from './module2.mjs'
-
-console.log(a);
-console.log(b);
-console.log(as);
-console.log(d);
--- src/module2.mjs
+++
@@ -1,8 +0,0 @@
-const a = "Deepak";
-const b = "Dinesh";
-const c = "Xavier";
-const d = "Naman";
-
-export default c;
-export {a, d};
-export {b};
